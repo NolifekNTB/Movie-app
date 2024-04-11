161565e9f5e3249f8aee875d3605de37d83385fc
NolifekNTB <kacperkotlowskikk1@gmail.com>
2024-04-11 09:41:42 +0200

    Calendar Screen clean code

---
+++ app/src/main/java/com/example/movieapp/features/Home/data/model/TopHitsAndNewSeasons.kt
@@ -0,0 +1,10 @@
+package com.example.movieapp.features.Home.data
+
+import com.example.movieapp.core.database.entities.AnimeItemNewSeasons
+import com.example.movieapp.core.database.entities.AnimeItemTopHits
+import kotlinx.coroutines.flow.Flow
+
+data class topHitsAndNewSeasons(
+    val topHits: Flow<List<AnimeItemTopHits>>,
+    val newSeasons: Flow<List<AnimeItemNewSeasons>>
+)
