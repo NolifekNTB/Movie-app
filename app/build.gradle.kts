3dc06571f05ca462e51aee1b90dd752c3b8edb34
NolifekNTB <kacperkotlowskikk1@gmail.com>
2024-04-17 10:09:55 +0200

    Migrate gradle to version catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,9 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android") version "1.9.0"
-    id("com.google.devtools.ksp") version "1.9.21-1.0.15"
-    id("org.jetbrains.kotlin.kapt")
-    id("com.google.dagger.hilt.android") version "2.46" apply false
-    id ("dagger.hilt.android.plugin")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainKotlin)
+    alias(libs.plugins.devToolsKSP)
+    alias(libs.plugins.kotlinKapt)
+    alias(libs.plugins.androidHilt)
 }
 
 
@@ -55,73 +54,54 @@ android {
     }
 }
 
-val coil = "2.6.0"
-val retrofit = "2.9.0"
-val okHttp = "4.12.0"
-val media3 = "1.2.1"
-val exoplayerExoPlayer = "2.19.1"
-val ygorluizfrazao = "1.0.0-alpha01"
-val daggerHilt = "2.50"
-val hiltCompose = "1.1.0"
-val room = "2.6.1"
-val navigationCompose = "2.7.7"
-val material = "1.6.1"
-val coreKtx = "1.12.0"
-val runtimeKtx = "2.7.0"
-val activityCompose = "1.8.2"
-val composeBom = "2023.08.00"
-val junit = "4.13.2"
-val extJunit = "1.1.5"
-val espressoCore = "3.5.1"
-
 dependencies {
     //Coil
-    implementation("io.coil-kt:coil-compose:$coil")
+    implementation(libs.coil.compose)
 
     //Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:$retrofit")
-    implementation ("com.squareup.retrofit2:converter-gson:$retrofit")
-    implementation ("com.squareup.okhttp3:okhttp:$okHttp")
-    implementation ("com.squareup.okhttp3:logging-interceptor:$okHttp")
+    implementation (libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.okhttp)
+    implementation (libs.logging.interceptor)
 
     //Media3 + Exoplayer
-    implementation ("androidx.media3:media3-exoplayer:$media3")
-    implementation ("androidx.media3:media3-ui:$media3")
-    implementation ("com.google.android.exoplayer:exoplayer:$exoplayerExoPlayer")
+    implementation (libs.media3.exoplayer)
+    implementation (libs.media3.ui)
+    implementation (libs.exoplayer)
 
     //Library to convert Icons from drawable
-    implementation ("com.github.ygorluizfrazao:compose-resources:$ygorluizfrazao")
+    implementation (libs.compose.resources)
 
     //Dagger Hilt
-    implementation ("com.google.dagger:hilt-android:$daggerHilt")
-    implementation("androidx.hilt:hilt-navigation-compose:$hiltCompose")
-    kapt ("com.google.dagger:hilt-compiler:$daggerHilt")
-    kapt ("androidx.hilt:hilt-compiler:$hiltCompose")
+    implementation (libs.hilt.android)
+    implementation(libs.hilt.navigation.compose)
+    kapt (libs.hilt.compiler)
+    kapt (libs.androidx.hilt.compiler)
 
     //ROOM
-    implementation("androidx.room:room-runtime:$room")
-    annotationProcessor("androidx.room:room-compiler:$room")
-    ksp("androidx.room:room-compiler:$room")
-    implementation("androidx.room:room-ktx:$room")
+    implementation(libs.androidx.room.runtime)
+    annotationProcessor(libs.androidx.room.compiler)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 
     //Navigation
-    implementation("androidx.navigation:navigation-compose:$navigationCompose")
-    implementation ("androidx.compose.material:material:$material")
+    implementation(libs.androidx.navigation.compose)
+    implementation (libs.androidx.material)
 
-    implementation("androidx.core:core-ktx:$coreKtx")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$runtimeKtx")
-    implementation("androidx.activity:activity-compose:$activityCompose")
-    implementation(platform("androidx.compose:compose-bom:$composeBom"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:$junit")
-    androidTestImplementation("androidx.test.ext:junit:$extJunit")
-    androidTestImplementation("androidx.test.espresso:espresso-core:$espressoCore")
-    androidTestImplementation(platform("androidx.compose:compose-bom:$composeBom"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
 
